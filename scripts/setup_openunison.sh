ce4002c5c3f688bcdea9d1a9cfa4a0a2c02cb127
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-08-23 22:27:40 +0000

    getting openunison working

--- scripts/setup_openunison.sh
+++ scripts/setup_openunison.sh
@@ -16,3 +16,23 @@ echo $OU_HOST
 echo $DB_HOST
 echo $API_HOST
 
+tput setaf 5
+echo -e "\n \n*******************************************************************************************************************"
+echo -e "Deploying the Kubernetes Dashboard"
+echo -e "*******************************************************************************************************************"
+tput setaf 3
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard -f ./kubernetes-dashboard/kubernetes-dashboard-values.yaml
+
+
+tput setaf 5
+echo -e "\n \n*******************************************************************************************************************"
+echo -e "Deploying ActiveDirectory (ApacheDS)"
+echo -e "*******************************************************************************************************************"
+tput setaf 3
+
+kubectl apply -f ./apacheds.yaml
+
+while [[ $(kubectl get pods -l app=apacheds -n activedirectory -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for apacheds to be running" && sleep 1; done
+
